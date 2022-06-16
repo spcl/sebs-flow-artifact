e90a315cb73b82c34b151a1284a2ff3166042692
Laurin Brandner <mail@laurinbrandner.ch>
2022-06-16 16:19:31 +0200

    Update graphs

--- vis/func_invo.py
+++ vis/func_invo.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -8,44 +8,52 @@ import matplotlib.ticker as ticker
 
 
 if __name__ == "__main__":
-    platforms = ["aws", "azure", "gcp"]
-    name = sys.argv[-1] if len(sys.argv) > 1 else "func_invo"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--experiments", nargs='+', default=["burst", "cold", "sequential", "warm"])
+    parser.add_argument("-p", "--platforms", nargs='+', default=["aws", "azure", "gcp"])
+    parser.add_argument("-m", "--memory", nargs='+', default=[])
+    args = parser.parse_args()
+
     func = "process"
-
     fig, ax = plt.subplots()
+    x_axis_len = []
 
-    for platform in platforms:
-        path = os.path.join("results", name, platform+".csv")
-        if not os.path.exists(path):
-            continue
+    for platform in args.platforms:
+        for experiment in args.experiments:
+            for memory in args.memory:
+                path = os.path.join("perf-cost", "620.func-invo", platform, f"{experiment}_{memory}.csv")
+                if not os.path.exists(path):
+                    continue
 
-        df = pd.read_csv(path)
-        reps = df["rep"].max()
+                df = pd.read_csv(path)
+                req_ids = df["request_id"].unique()
 
-        ys = []
-        for i in range(1, reps+1):
-            start = df.loc[((df["func"] == func) & (df["rep"] == i))].sort_values(["start"])["start"].to_numpy()
-            end = df.loc[((df["func"] == func) & (df["rep"] == i))].sort_values(["end"])["end"].to_numpy()
+                ys = []
+                for id in req_ids:
+                    start = df.loc[((df["func"] == func) & (df["request_id"] == id))].sort_values(["start"])["start"].to_numpy()
+                    end = df.loc[((df["func"] == func) & (df["request_id"] == id))].sort_values(["end"])["end"].to_numpy()
 
-            # sanity checks to verify no functions are overlapping
-            assert(np.all(start[:-1] < start[1:]))
-            assert(np.all(end[:-1] < end[1:]))
-            assert(np.all(end[:-1] < start[1:]))
+                    # sanity checks to verify no functions are overlapping
+                    assert(np.all(start[:-1] < start[1:]))
+                    assert(np.all(end[:-1] < end[1:]))
+                    assert(np.all(end[:-1] < start[1:]))
 
-            ys.append(start[1:] - end[:-1])
+                    ys.append(start[1:] - end[:-1])
 
-        ys = np.asarray(ys)
-        ys = np.mean(ys, axis=1)
-        xs = np.arange(ys.shape[0])
+                ys = np.asarray(ys)
+                ys = np.mean(ys, axis=1)
+                xs = np.arange(ys.shape[0])
 
-        line = ax.plot(xs, ys)[0]
-        line.set_label(platform)
+                line = ax.plot(xs, ys)[0]
+                line.set_label(f"{platform}_{experiment}_{memory}")
+                x_axis_len.append(len(xs))
 
     ax.set_title("function invocation")
     ax.set_xlabel("repetition")
     ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
     ax.set_xticks(np.arange(0, len(xs)+1, 5))
     ax.set_ylabel("latency [s]")
+    ax.set_xlim([0, min(x_axis_len)-1])
     fig.legend()
 
     plt.show()
--- vis/runtime.py
+++ vis/runtime.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -7,37 +7,90 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 
-if __name__ == "__main__":
-    platforms = ["aws", "azure", "gcp"]
-    name = sys.argv[-1] if len(sys.argv) > 1 else "parallel"
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--benchmark", type=str)
+parser.add_argument("-e", "--experiments", nargs='+', default=["burst", "cold", "sequential", "warm"])
+parser.add_argument("-p", "--platforms", nargs='+', default=["aws", "azure", "gcp"])
+parser.add_argument("-m", "--memory", nargs='+', default=[])
+parser.add_argument("--mean", action="store_true", default=False)
+args = parser.parse_args()
 
+
+def bar_plot():
     fig, ax = plt.subplots()
+    w = 0.3
+    xs = np.arange(len(args.experiments))
 
-    for platform in platforms:
-        path = os.path.join("results", name, platform+".csv")
-        if not os.path.exists(path):
-            continue
+    for idx, platform in enumerate(args.platforms):
+        ys = []
+        es = []
 
-        df = pd.read_csv(path)
+        for experiment in args.experiments:
+            for memory in args.memory:
+                filename = f"{experiment}_{memory}.csv" if platform != "azure" else f"{experiment}.csv"
+                path = os.path.join("perf-cost", args.benchmark, platform, filename)
+                if not os.path.exists(path):
+                    ys.append(0)
+                    es.append(0)
+                    continue
 
-        generate = df.loc[(df["func"] == "generate")].set_index("rep")
-        process = df.loc[(df["func"] == "process")].groupby("rep")
-        d_total = process["end"].max() - generate["start"]
+                df = pd.read_csv(path)
+                invocs = df.groupby("request_id")
+                d_total = invocs["end"].max() - invocs["start"].min()
 
-        ys = np.asarray(d_total)
-        xs = np.arange(ys.shape[0])
+                ys.append(np.mean(d_total))
+                es.append(np.std(d_total))
 
-        line = ax.plot(xs, ys)[0]
-        line.set_label(platform)
+        o = ((len(args.platforms)-1)*w)/2.0 - idx*w
+        ax.bar(xs-o, ys, w, label=platform, yerr=es, capsize=3)
 
-        ys = ys[np.where(~np.isnan(ys))]
-        print(platform, "std:", np.std(ys))
-
-    ax.set_title("runtime")
-    ax.set_xlabel("repetition")
-    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-    ax.set_xticks(np.arange(0, len(xs)+1, 5))
+    ax.set_title(f"{args.benchmark} runtime")
     ax.set_ylabel("duration [s]")
+    ax.set_xticks(xs, args.experiments)
     fig.legend()
 
     plt.show()
+
+
+def line_plot():
+    fig, ax = plt.subplots()
+    x_axis_len = []
+
+    for platform in args.platforms:
+        for experiment in args.experiments:
+            for memory in args.memory:
+                filename = f"{experiment}_{memory}.csv" if platform != "azure" else f"{experiment}.csv"
+                path = os.path.join("perf-cost", args.benchmark, platform, filename)
+                if not os.path.exists(path):
+                    continue
+
+                df = pd.read_csv(path)
+                invocs = df.groupby("request_id")
+                d_total = invocs["end"].max() - invocs["start"].min()
+
+                ys = np.asarray(d_total)
+                xs = np.arange(ys.shape[0])
+
+                line = ax.plot(xs, ys)[0]
+                line.set_label(f"{platform}_{experiment}_{memory}")
+
+                ys = ys[np.where(~np.isnan(ys))]
+                print(platform, "std:", np.std(ys))
+                x_axis_len.append(len(xs))
+
+    ax.set_title(f"{args.benchmark} runtime")
+    ax.set_xlabel("repetition")
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    ax.set_xticks(np.arange(0, min(x_axis_len)+1, 5))
+    ax.set_ylabel("duration [s]")
+    ax.set_xlim([0, min(x_axis_len)-1])
+    fig.legend()
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    if args.mean:
+        bar_plot()
+    else:
+        line_plot()
