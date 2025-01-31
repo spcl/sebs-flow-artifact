b1d447949ad45b6ff45d3c0e73544fad4cb5e71e
Larissa Schmid <larissa.schmid@kit.edu>
2025-01-31 16:57:37 +0100

    add filename to overhead_storage analysis

--- analysis/overhead_storage.py
+++ analysis/overhead_storage.py
@@ -57,6 +57,7 @@ def line_plot():
     for platform in args.platforms:
         for size in args.sizes:
             for experiment in args.experiments:
+                filename = f"{experiment}_512.csv"
                 path = os.path.join("./../perf-cost", "631.parallel-download", f"{platform}_{size}", filename)
                 
                 if not os.path.exists(path):
